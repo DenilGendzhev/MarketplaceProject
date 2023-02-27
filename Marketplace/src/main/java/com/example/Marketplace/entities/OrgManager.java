b02d43f9b463c10e310debe307b2be9fca59e745
DenilGEndzhev <denilgendjev@gmail.com>
2023-02-27 21:10:26 +0200

    added to org and user

--- Marketplace/src/main/java/com/example/Marketplace/entities/OrgManager.java
+++ Marketplace/src/main/java/com/example/Marketplace/entities/OrgManager.java
@@ -1,16 +1,19 @@
 package com.example.Marketplace.entities;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Size;
 
 @Entity
+@Table (name="orgmanager")
 public class OrgManager {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-    private String orgName;
+    @Size(min=2, max=30)
+    private String title;
+    @Email
+    private String email;
 
 
 
--- Marketplace/src/main/java/com/example/Marketplace/entities/User.java
+++ Marketplace/src/main/java/com/example/Marketplace/entities/User.java
@@ -1,4 +1,20 @@
 package com.example.Marketplace.entities;
 
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "user")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Size(min=2, max=30)
+    private String firstName;
+    @Size(min=2, max=30)
+    private String lastName;
+    @Email
+    private String email;
+
 }
