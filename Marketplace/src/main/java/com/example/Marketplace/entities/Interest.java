06d1bf19fe21e2f9b6e01a2867dc542a51d03fe4
DenilGEndzhev <denilgendjev@gmail.com>
2023-02-28 21:15:19 +0200

    added interest entity

---
+++ Marketplace/src/main/java/com/example/Marketplace/entities/Interest.java
@@ -0,0 +1,14 @@
+package com.example.Marketplace.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Interest {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String interestName;
+}
