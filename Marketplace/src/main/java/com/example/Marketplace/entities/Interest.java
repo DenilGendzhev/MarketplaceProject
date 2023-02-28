b9ce6e00306041e452c047b418f6fed9afbcd5c6
DenilGEndzhev <denilgendjev@gmail.com>
2023-02-28 21:19:32 +0200

    added interest entity and getters setters

--- Marketplace/src/main/java/com/example/Marketplace/entities/Interest.java
+++ Marketplace/src/main/java/com/example/Marketplace/entities/Interest.java
@@ -11,4 +11,20 @@ public class Interest {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String interestName;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getInterestName() {
+        return interestName;
+    }
+
+    public void setInterestName(String interestName) {
+        this.interestName = interestName;
+    }
 }
