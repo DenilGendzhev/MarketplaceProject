82d967cc240def8569ddd82c768db3eac6cecf3a
DenilGEndzhev <denilgendjev@gmail.com>
2023-04-05 20:01:21 +0300

    org adID added

--- Marketplace/src/main/java/com/example/Marketplace/entities/OrgManager.java
+++ Marketplace/src/main/java/com/example/Marketplace/entities/OrgManager.java
@@ -2,7 +2,6 @@ package com.example.Marketplace.entities;
 
 import javax.persistence.*;
 import javax.validation.constraints.Email;
-import javax.validation.constraints.Size;
 
 @Entity
 @Table (name="orgmanager")
@@ -14,11 +13,11 @@ public class OrgManager {
     @Email
     private String email;
     private String password;
+    private int adID;
 
     public Integer getId() {
         return id;
     }
-
     public void setId(Integer id) {
         this.id = id;
     }
@@ -31,16 +30,19 @@ public class OrgManager {
     public String getEmail() {
         return email;
     }
-
     public void setEmail(String email) {
         this.email = email;
     }
-
     public String getPassword() {
         return password;
     }
-
     public void setPassword(String password) {
         this.password = password;
+    }
+    public int getAdID() {
+        return adID;
+    }
+    public void setAdID(int adID) {
+        this.adID = adID;
     }
 }
