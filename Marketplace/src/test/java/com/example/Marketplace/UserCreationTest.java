4d911081bf583e2bc8d4610a7f75aaa6f6fc07f3
DenilGEndzhev <denilgendjev@gmail.com>
2023-04-08 14:35:20 +0300

    user creation test

---
+++ Marketplace/src/test/java/com/example/Marketplace/UserCreationTest.java
@@ -0,0 +1,23 @@
+package com.example.Marketplace;
+
+import com.example.Marketplace.controllers.UserController;
+
+import com.example.Marketplace.entities.User;
+import com.example.Marketplace.enums.Role;
+import com.example.Marketplace.repositories.UserRepository;
+import com.example.Marketplace.services.UserService;
+import org.junit.jupiter.api.Test;
+
+public class UserCreationTest {
+@Test
+    public void testCreateUser() {
+    User user = new User();
+    user.setEmail("test@gmail.com");
+    user.setFirstName("Test");
+    user.setLastName("Name");
+    user.setPassword("123456789");
+    user.setRole(Role.USER);
+
+    //User savedUser = UserRepository.save(user);
+}
+}
